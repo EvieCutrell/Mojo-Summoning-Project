0f693ba397abfacb336c13328d084a587b2ac739
eviecutrell <evelynn.cutrell@alaskaair.com>
2024-02-20 09:18:30 -0800

    updated index.js with associations

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,6 +5,14 @@ const Attack = require("./Attack.js")
 // import the rest of your models above
 
 // set up the associations here
+User.hasOne(Deck);
+Deck.belongsTo(User);
+
+Deck.hasMany(Card);
+Card.belongsTo(Deck);
+
+Card.belongsToMany(Attack);
+Attack.belongsToMany(Card);
 
 // and then export them all below
 module.exports = 
